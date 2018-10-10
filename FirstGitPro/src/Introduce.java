7d0e2251e4ecab746cce8f67c87fb2fb3fee8177
wushichang <42518274+wushichang@users.noreply.github.com>
2018-10-10 14:55:48 +0800

    Update Introduce.java
    change from internet

--- FirstGitPro/src/Introduce.java
+++ FirstGitPro/src/Introduce.java
@@ -2,4 +2,5 @@
 public class Introduce {
 	private String operation1="it is my first git project,liyuanqian is comming";
 	private String operation2="this change is from local";
+	private String operation3="i am single lady";
 }
